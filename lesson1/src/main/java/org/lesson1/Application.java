ac73e13b606971627da94d9f0da21d1302a1d5b2
PolinaAlimova11 <catcheshire07@gmail.com>
2024-07-21 16:05:37 +0300

    test commit

--- lesson1/src/main/java/org/lesson1/Application.java
+++ lesson1/src/main/java/org/lesson1/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext("org.lesson1");
         System.out.println("Init");
+        System.out.println("Test");
 
     }
 }
